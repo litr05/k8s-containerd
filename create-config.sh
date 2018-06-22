612b8f99958a82e3833850be65c8351db9a7739e
rodionov <rodionov@mbk-capital.ru>
2018-06-22 12:31:36 +0300

    change config for 3 masters

--- create-config.sh
+++ create-config.sh
@@ -3,13 +3,13 @@
 # local machine ip address
 export K8SHA_IPLOCAL=172.26.133.21
 
-# local machine etcd name, options: etcd1, etcd2, etcd3, etcd4, etcd5
+# local machine etcd name, options: etcd1, etcd2, etcd3
 export K8SHA_ETCDNAME=etcd1
 
 # local machine keepalived state config, options: MASTER, BACKUP. One keepalived cluster only one MASTER, other's are BACKUP
 export K8SHA_KA_STATE=MASTER
 
-# local machine keepalived priority config, options: 102, 101, 100, 99, 98. MASTER must 102
+# local machine keepalived priority config, options: 102, 101,100 MASTER must 102
 export K8SHA_KA_PRIO=102
 
 # local machine keepalived network interface name config, for example: eth0
@@ -31,12 +31,6 @@ export K8SHA_IP2=172.26.133.22
 # master03 ip address
 export K8SHA_IP3=172.26.133.23
 
-# master04 ip address
-export K8SHA_IP4=172.26.133.24
-
-# master05 ip address
-export K8SHA_IP5=172.26.133.25
-
 # master01 hostname
 export K8SHA_HOSTNAME1=hb-master01
 
@@ -46,20 +40,15 @@ export K8SHA_HOSTNAME2=hb-master02
 # master03 hostname
 export K8SHA_HOSTNAME3=hb-master03
 
-# master04 hostname
-export K8SHA_HOSTNAME4=hb-master04
-
-# master04 hostname
-export K8SHA_HOSTNAME5=hb-master05
-
 # keepalived auth_pass config, all masters must be same
 export K8SHA_KA_AUTH=56cf8dd754c90194d1600c483e10abfr
 
 #etcd tocken:
-export ETCD_TOKEN=9489bf68bdfe1b9ae037d6fd9e7efefd
+export ETCD_TOKEN=9489bf67bdfe1b3ae077d6fd9e7efefd
 
 # kubernetes cluster token, you can use 'kubeadm token generate' to get a new one
-export K8SHA_TOKEN=yf0t14.7ekahohaetbdphqg
+export K8SHA_TOKEN=535tdi.utzk5hf75b04ht8l
+
 
 # kubernetes CIDR pod subnet, if CIDR pod subnet is "10.244.0.0/16" please set to "10.244.0.0\\/16"
 export K8SHA_CIDR=10.244.0.0\\/16
@@ -76,8 +65,6 @@ sed \
 -e "s/K8SHA_IP1/$K8SHA_IP1/g" \
 -e "s/K8SHA_IP2/$K8SHA_IP2/g" \
 -e "s/K8SHA_IP3/$K8SHA_IP3/g" \
--e "s/K8SHA_IP4/$K8SHA_IP4/g" \
--e "s/K8SHA_IP5/$K8SHA_IP5/g" \
 -e "s/ETCD_TOKEN/$ETCD_TOKEN/g" \
 etcd/docker-compose.yaml.tpl > etcd/docker-compose.yaml
 
@@ -104,8 +91,6 @@ sed \
 -e "s/K8SHA_IP1/$K8SHA_IP1/g" \
 -e "s/K8SHA_IP2/$K8SHA_IP2/g" \
 -e "s/K8SHA_IP3/$K8SHA_IP3/g" \
--e "s/K8SHA_IP4/$K8SHA_IP4/g" \
--e "s/K8SHA_IP5/$K8SHA_IP5/g" \
 nginx-lb/nginx-lb.conf.tpl > nginx-lb/nginx-lb.conf
 
 echo 'set nginx load balancer config file success: nginx-lb/nginx-lb.conf'
@@ -115,13 +100,9 @@ sed \
 -e "s/K8SHA_HOSTNAME1/$K8SHA_HOSTNAME1/g" \
 -e "s/K8SHA_HOSTNAME2/$K8SHA_HOSTNAME2/g" \
 -e "s/K8SHA_HOSTNAME3/$K8SHA_HOSTNAME3/g" \
--e "s/K8SHA_HOSTNAME4/$K8SHA_HOSTNAME4/g" \
--e "s/K8SHA_HOSTNAME5/$K8SHA_HOSTNAME5/g" \
 -e "s/K8SHA_IP1/$K8SHA_IP1/g" \
 -e "s/K8SHA_IP2/$K8SHA_IP2/g" \
 -e "s/K8SHA_IP3/$K8SHA_IP3/g" \
--e "s/K8SHA_IP4/$K8SHA_IP4/g" \
--e "s/K8SHA_IP5/$K8SHA_IP5/g" \
 -e "s/K8SHA_IPVIRTUAL/$K8SHA_IPVIRTUAL/g" \
 -e "s/K8SHA_TOKEN/$K8SHA_TOKEN/g" \
 -e "s/K8SHA_CIDR/$K8SHA_CIDR/g" \
@@ -135,4 +116,3 @@ kube-ingress/service-nodeport.yaml.tpl > kube-ingress/service-nodeport.yaml
 
 
 echo 'set kubeadm init config file success: kubeadm-init.yaml'
-
--- kubeadm-init.yaml.tpl
+++ kubeadm-init.yaml.tpl
@@ -1,19 +1,18 @@
 apiVersion: kubeadm.k8s.io/v1alpha1
 kind: MasterConfiguration
 kubernetesVersion: v1.10.3
+api:
+  advertiseAddress: K8SHA_IPVIRTUAL
+  controlPlaneEndpoint: K8SHA_IPVIRTUAL
 networking:
   podSubnet: K8SHA_CIDR
 apiServerCertSANs:
 - K8SHA_HOSTNAME1
 - K8SHA_HOSTNAME2
 - K8SHA_HOSTNAME3
-- K8SHA_HOSTNAME4
-- K8SHA_HOSTNAME5
 - K8SHA_IP1
 - K8SHA_IP2
 - K8SHA_IP3
-- K8SHA_IP4
-- K8SHA_IP5
 - K8SHA_IPVIRTUAL
 - 127.0.0.1
 etcd:
@@ -21,7 +20,7 @@ etcd:
   - http://K8SHA_IP1:2379
   - http://K8SHA_IP2:2379
   - http://K8SHA_IP3:2379
-  - http://K8SHA_IP4:2379
-  - http://K8SHA_IP5:2379
+apiServerExtraArgs:
+  apiserver-count: "3"
 token: K8SHA_TOKEN
 tokenTTL: "0"
